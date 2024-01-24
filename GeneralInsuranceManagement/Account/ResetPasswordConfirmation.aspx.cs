d4a3ec643d6b4089c29277e9fd887df310430a06
Malcolm <machirimalcolm@gmail.com>
2024-01-24 17:01:49 +0200

    --initial commit

---
+++ GeneralInsuranceManagement/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GeneralInsuranceManagement.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ GeneralInsuranceManagement/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GeneralInsuranceManagement.Startup))]
+namespace GeneralInsuranceManagement
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
