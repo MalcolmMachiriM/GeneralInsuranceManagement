d06b5cd97793d7058e9f1e9af14904b5f441f2c9
Malcolm <machirimalcolm@gmail.com>
2024-02-09 11:06:57 +0200

    underwriting done

--- GeneralInsuranceManagement/GlobalParameters/UnderwritingQuestions/UnderwritingQuestionsCreate.aspx.cs
+++ GeneralInsuranceManagement/GlobalParameters/UnderwritingQuestions/UnderwritingQuestionsCreate.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using GeneralInsuranceManagement.Models.GlobalParameters;
 
 namespace GeneralInsuranceManagement.GlobalParameters.UnderwritingQuestions
 {
@@ -11,7 +12,58 @@ namespace GeneralInsuranceManagement.GlobalParameters.UnderwritingQuestions
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["underwritingQuestionId"] !=null )
+                {
+                    underwritingQuestionId.Value = Request.QueryString["underwritingQuestionId"];
+                }
+            }
+        }
+        #region alerts
+        protected void RedAlert(string MsgFlg)
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(), "showSuccess", "Swal.fire('Error!', '" + MsgFlg + "', 'error');", true);
 
+        }
+
+        protected void WarningAlert(string MsgFlg)
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(), "showSuccess", "Swal.fire('Warning!', '" + MsgFlg + "', 'warning');", true);
+
+        }
+
+        protected void SuccessAlert(string MsgFlg)
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(), "showSuccess", "Swal.fire('Success!', '" + MsgFlg + "', 'success');", true);
+
+        }
+        #endregion
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void Save()
+        {
+            Models.GlobalParameters.UnderwritingQuestions qs = new Models.GlobalParameters.UnderwritingQuestions("cn", 1)
+            {
+                Id = int.Parse(underwritingQuestionId.Value),
+                QuestionType = QuestionTypes.Text,
+                QuestionDescription = QuestionDescription.Text
+            };
+            try
+            {
+                if (qs.Save())
+                {
+                    SuccessAlert("Success");
+                }
+            }
+            catch (Exception x)
+            {
+
+                RedAlert(x.Message);
+            }
         }
     }
 }
--- GeneralInsuranceManagement/GlobalParameters/UnderwritingQuestions/UnderwritingQuestionsCreate.aspx.designer.cs
+++ GeneralInsuranceManagement/GlobalParameters/UnderwritingQuestions/UnderwritingQuestionsCreate.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace GeneralInsuranceManagement.GlobalParameters.UnderwritingQuestions
     {
 
         /// <summary>
-        /// SchemeId control.
+        /// underwritingQuestionId control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HiddenField SchemeId;
+        protected global::System.Web.UI.WebControls.HiddenField underwritingQuestionId;
 
         /// <summary>
         /// ErrorMessage control.
