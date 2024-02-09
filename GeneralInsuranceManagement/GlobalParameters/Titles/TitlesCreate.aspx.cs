f494631c572b65c50f024307384e5019a66ccdf1
Malcolm <machirimalcolm@gmail.com>
2024-02-09 11:33:07 +0200

    titles stuff

--- GeneralInsuranceManagement/GlobalParameters/Titles/TitlesCreate.aspx.cs
+++ GeneralInsuranceManagement/GlobalParameters/Titles/TitlesCreate.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeneralInsuranceManagement.Models.GlobalParameters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,51 @@ namespace GeneralInsuranceManagement.GlobalParameters.Titles
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+        #region alerts
+        protected void RedAlert(string MsgFlg)
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(), "showSuccess", "Swal.fire('Error!', '" + MsgFlg + "', 'error');", true);
+
+        }
+
+        protected void WarningAlert(string MsgFlg)
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(), "showSuccess", "Swal.fire('Warning!', '" + MsgFlg + "', 'warning');", true);
+
+        }
+
+        protected void SuccessAlert(string MsgFlg)
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(), "showSuccess", "Swal.fire('Success!', '" + MsgFlg + "', 'success');", true);
+
+        }
+        #endregion
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            SaveTitle();
+        }
+
+        private void SaveTitle()
+        {
+            Models.GlobalParameters.Titles title = new Models.GlobalParameters.Titles("cn", 1)
+            {
+                Id = int.Parse(TitleId.Value),
+                Name = Titles.Text
+            };
+            try
+            {
+                if (title.Save())
+                {
+                    SuccessAlert("Title saved");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                RedAlert(ex.Message);
+            }
 
         }
     }
